71faf81fff60bb0955b2d45002c9440a0587376d
Caiden Romero <caidenromero@caiden-mbp.local>
2024-02-03 14:13:59 -0500

    adds bottom nav bar

--- hackviolet_project/lib/main.dart
+++ hackviolet_project/lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(const TabBarDemo());
 }
 
-
 class TabBarDemo extends StatelessWidget {
   const TabBarDemo({super.key});
 
@@ -31,8 +30,62 @@ class TabBarDemo extends StatelessWidget {
               Icon(Icons.account_circle),
             ],
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Topics',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.apps),
+                label: 'About',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle),
+                label: 'Profile',
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+// class BottomNavBar extends StatelessWidget {
+//   const BottomNavBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       items: const [
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home),
+//           label: 'Topics',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.apps),
+//           label: 'About',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.account_circle),
+//           label: 'Profile',
+//         ),
+//       ],
+//       fixedColor: Colors.deepPurple[200],
+//       onTap: (int idx) {
+//         switch (idx) {
+//           case 0:
+//             // do nothing
+//             break;
+//           case 1:
+//             Navigator.pushNamed(context, '/about');
+//             break;
+//           case 2:
+//             Navigator.pushNamed(context, '/profile');
+//             break;
+//         }
+//       },
+//     );
+//   }
+// }
